b7682dcc500b222526c0de9eae630c6bda0b6b17
samsu <samsu@samsu.id>
2024-07-26 15:27:57 +0700

    asdasd

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from users.views import (CreateUser,CekDokumen,LoginUsers,ProfileUsers, UpdateProfile,FollowUser,LogoutUsers,ListFollowers,
+from users.views import (CreateUser,LoginUsers,ProfileUsers, UpdateProfile,FollowUser,LogoutUsers,ListFollowers,
 SearchingAcocunt)
 
 urlpatterns = [
 	path('register',CreateUser.as_view()),
 	path('login',LoginUsers.as_view()),
 	path('logout',LogoutUsers.as_view()),
-	path('dokumen/<str:id>',CekDokumen.as_view()),
 	path('profile/<str:pk>',ProfileUsers.as_view()),
 	path('profile/update/<int:pk>',UpdateProfile.as_view()),
 	path('follow/<int:pk>',FollowUser.as_view()),
